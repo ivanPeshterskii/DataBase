2663309b35c5659d5be7f44503ba83754b8d5cfd
Ivan Pehsterski <peshterskiivan@gmail.com>
2026-05-17 20:04:18 +0300

    add: Solution for part three Diablo DB

---
+++ 05.CRUD-Exercise/Solution/SolutionPartThree.sql
@@ -0,0 +1,10 @@
+----------------------------
+--   SOLUTIONS
+-- Part III – Queries for Diablo Database
+----------------------------
+
+-- 25.	All Diablo Characters
+SELECT 
+    [Name]
+FROM Characters
+ORDER BY [Name] ASC;
